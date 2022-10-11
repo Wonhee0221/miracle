ea4ea0fa57fe4c5d293835163a2a1eb8a52a2815
Wonhee0221 <91041488+Wonhee0221@users.noreply.github.com>
2022-10-11 21:02:21 +0900

    Add files via upload

--- middle/model/Ranker_user.dart
+++ middle/model/Ranker_user.dart
@@ -9,10 +9,7 @@ class Ranker_User extends StatefulWidget {
   _Ranker_UserState createState() => _Ranker_UserState();
 }
 
-class User_inpo{
-
-}
-
+class User_inpo {}
 
 class _Ranker_UserState extends State<Ranker_User> {
   late List<Users> titles;
@@ -31,67 +28,63 @@ class _Ranker_UserState extends State<Ranker_User> {
     carbonDate = titles.map((m) => m.carbonData).cast<int>().toList();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(7),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Container(
-            height: 300,
-            child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                //좌우로 스크롤 할수 있게해줌, 위아래는 vertical
-                children: mostrankList(context, titles)),
-          ),
+              height: 150,
+              child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  children: mostrankList(context, titles))),
         ],
       ),
     );
   }
 
-
   List<Widget> mostrankList(BuildContext context, List<Users> titles) {
     List<Widget> rankresults = [];
     for (var i = 0; i < 3; i++) {
-      rankresults.add(
-          Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 5),
-                child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: NetworkImage(titles[i].profile)),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 10),
-                child: Text('닉네임 : ' + titles[i].name,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.black),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 10),
-                child: Text((i + 1).toString() + '위',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                      color: Colors.black),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(15),
-                width: 100,
-                height: 5,
-                color: Colors.amber,
-              ),
-            ],
-          )
-      );
+      rankresults.add(Container(
+          child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 5),
+            child: CircleAvatar(
+                radius: 30, backgroundImage: NetworkImage(titles[i].profile)),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            child: Text(
+              '닉네임 : ' + titles[i].name,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13,
+                  color: Colors.black),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            child: Text(
+              (i + 1).toString() + '위',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.black),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(15),
+            width: 70,
+            height: 5,
+            color: Colors.amber,
+          ),
+        ],
+      )));
     }
     return rankresults;
   }
